d85b239a2669363634128df5b3eeddca65029288
CarlJlin <jeanlincarlpro@gmail.com>
2025-02-25 16:16:22 +0100

    Initial commit after the setup of libGDX

---
+++ android/src/main/java/fr/CarlJlin/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package fr.CarlJlin.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.CarlJlin.GameMain;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new GameMain(), configuration);
+    }
+}
